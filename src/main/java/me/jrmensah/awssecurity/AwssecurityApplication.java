644588a27c9c8474e1332877f7517c4657f913d2
jrmensah <jerilynmensah@gmail.com>
2017-11-21 10:00:25 -0500

    Security with Amazon AWS

---
+++ src/main/java/me/jrmensah/awssecurity/AwssecurityApplication.java
@@ -0,0 +1,12 @@
+package me.jrmensah.awssecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwssecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwssecurityApplication.class, args);
+	}
+}
---
+++ src/test/java/me/jrmensah/awssecurity/AwssecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package me.jrmensah.awssecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwssecurityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
